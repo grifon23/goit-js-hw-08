cb1d0b645181564e1ded9147719018ae4d72a6b3
grifon23 <shuklin.eduard.ua@gmail.com>
2022-03-20 22:12:55 +0200

    comolete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
 console.log(galleryItems);
+
+const makeGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+  <li>
+    <a class="gallery__item" href="${original}">
+    <img 
+    style="display:block"
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+  </a>
+  </li>`;
+};
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+player.on(
+  'timeupdate',
+  throttle(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+  }, 1000),
+);
+const storage = localStorage.getItem('videoplayer-current-time');
+const setCurrentTime = () => {
+  if (!storage) return;
+  player.setCurrentTime(storage);
+};
+
+setCurrentTime();
